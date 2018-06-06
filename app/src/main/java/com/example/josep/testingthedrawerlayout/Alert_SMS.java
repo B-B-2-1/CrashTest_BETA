8f9f95c06e09288105814737818cab53e6708f15
B-B-2-1 <josephbaby.baby@gmail.com>
2018-06-06 19:24:22 +0530

    while (latitude==null || longitude==null){}

--- app/src/main/java/com/example/josep/testingthedrawerlayout/Alert_SMS.java
+++ app/src/main/java/com/example/josep/testingthedrawerlayout/Alert_SMS.java
@@ -92,7 +92,6 @@ public class Alert_SMS extends Activity {
 
                 while (latitude==null || longitude==null){
 
-
                 }
 
                 for(int i=0;i<tinyDB.getListString("NumbersOnly").size();i++) {
